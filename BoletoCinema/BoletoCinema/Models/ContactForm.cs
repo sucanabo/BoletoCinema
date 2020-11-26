aa750b527391b3961bdc53024d127f95366a69a8
Phuc <sucanabo@gmail.com>
2020-11-26 20:57:46 +0700

    Model by Trinh

---
+++ BoletoCinema/BoletoCinema/Models/ContactForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoletoCinema.Models
+{
+    public class ContactForm
+    {
+        [Key]
+        public int id { get; set; }
+        public int user_id { get; set; }
+        public string content { get; set; }
+        public string status { get; set; }
+        public string create_date { get; set; }
+        [ForeignKey("user_id")]
+        //public virtual User user { get; set; }
+        
+    }
+}
---
+++ BoletoCinema/BoletoCinema/Models/Report.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoletoCinema.Models
+{
+    public class Report
+    {
+        [Key]
+        public int id { get; set; }
+        public string reason { get; set; }
+        public string content { get; set; }
+        public string create_date { get; set; }
+        public string status { get; set; }
+        public virtual Review review { get; set; }
+    }
+}
