abb252a515cea103fa215e448c32e87709c48071
xuanloc <xuanloc.hcm2018@gmail.com>
2020-11-27 09:34:59 +0700

    Model by springLog

---
+++ BoletoCinema/BoletoCinema/Models/Branch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace BoletoCinema.Models
+{
+    public class Branch
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string address { get; set; }
+
+        public string status { get; set; }
+
+        public ICollection<Schedule> schedule { get; set; }
+
+        public ICollection<Room> room { get; set; }
+    }
+}
---
+++ BoletoCinema/BoletoCinema/Models/Seat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace BoletoCinema.Models
+{
+    public class Seat
+    {
+        public int id { get; set; }
+
+        public int room_id { get; set; }
+
+        public string position { get; set; }
+
+        public int status { get; set; }
+
+        [ForeignKey("room_id")]
+        public virtual Room room { get; set; }
+
+       // public ICollection<Ticket> ticket { get; set; }
+    }
+}
