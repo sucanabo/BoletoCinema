54a53733b9dfcd3dfb230aff100fa3ff8bf9e804
Kane-TM <0306181335@caothang.edu.vn>
2020-11-27 09:33:33 +0700

    movie_session_order_ticket_models

---
+++ BoletoCinema/BoletoCinema/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoletoCinema.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public  string summery { get; set; }
+
+        public  string trailer_path { get; set; }
+
+        public  decimal price { get; set; }
+
+        public int status { get; set; }
+
+        public int duration { get; set; }
+
+        public DateTime published_date { get; set; }
+
+        public ICollection<Session> ListSession { get; set; }
+    }
+}
---
+++ BoletoCinema/BoletoCinema/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoletoCinema.Models
+{
+    public class Order
+    {
+        [Key]
+        public int id { get; set; }
+
+        public int user_id { get; set; }
+        [ForeignKey("user_id")]
+        public virtual User user { get; set; }
+
+        public int ticket_id { get; set; }
+        [ForeignKey("ticket_id")]
+        public virtual Ticket ticket { get; set; }
+
+        public decimal total_price { get; set; }
+
+        public int status { get; set; }
+
+        public DateTime create_date { get; set; }
+    }
+}
