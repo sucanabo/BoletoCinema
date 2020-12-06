dee0ceb289e6523ff3168b143a071f1d9b193969
Dustin <kiendf@gmail.com>
2020-12-06 23:58:15 +0700

    create discount/user/schedule controller with view

---
+++ BoletoCinema/BoletoCinema/Areas/Admin/Controllers/DiscountsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BoletoCinema.Areas.Admin.Data;
+using BoletoCinema.Areas.Admin.Models;
+
+namespace BoletoCinema.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DiscountsController : Controller
+    {
+        private readonly BoletoContext _context;
+
+        public DiscountsController(BoletoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Discounts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.discounts.ToListAsync());
+        }
+
+        // GET: Admin/Discounts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var discount = await _context.discounts
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            return View(discount);
+        }
+
+        // GET: Admin/Discounts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Discounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,name,discount_percent,status")] Discount discount)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(discount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(discount);
+        }
+
+        // GET: Admin/Discounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var discount = await _context.discounts.FindAsync(id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+            return View(discount);
+        }
+
+        // POST: Admin/Discounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,name,discount_percent,status")] Discount discount)
+        {
+            if (id != discount.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(discount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DiscountExists(discount.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(discount);
+        }
+
+        // GET: Admin/Discounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var discount = await _context.discounts
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            return View(discount);
+        }
+
+        // POST: Admin/Discounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var discount = await _context.discounts.FindAsync(id);
+            _context.discounts.Remove(discount);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DiscountExists(int id)
+        {
+            return _context.discounts.Any(e => e.id == id);
+        }
+    }
+}
---
+++ BoletoCinema/BoletoCinema/Areas/Admin/Controllers/SchedulesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BoletoCinema.Areas.Admin.Data;
+using BoletoCinema.Areas.Admin.Models;
+
+namespace BoletoCinema.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SchedulesController : Controller
+    {
+        private readonly BoletoContext _context;
+
+        public SchedulesController(BoletoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Schedules
+        public async Task<IActionResult> Index()
+        {
+            var boletoContext = _context.schedules.Include(s => s.branch);
+            return View(await boletoContext.ToListAsync());
+        }
+
+        // GET: Admin/Schedules/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.schedules
+                .Include(s => s.branch)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return View(schedule);
+        }
+
+        // GET: Admin/Schedules/Create
+        public IActionResult Create()
+        {
+            ViewData["branch_id"] = new SelectList(_context.branches, "id", "id");
+            return View();
+        }
+
+        // POST: Admin/Schedules/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,branch_id")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(schedule);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["branch_id"] = new SelectList(_context.branches, "id", "id", schedule.branch_id);
+            return View(schedule);
+        }
+
+        // GET: Admin/Schedules/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.schedules.FindAsync(id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+            ViewData["branch_id"] = new SelectList(_context.branches, "id", "id", schedule.branch_id);
+            return View(schedule);
+        }
+
+        // POST: Admin/Schedules/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,branch_id")] Schedule schedule)
+        {
+            if (id != schedule.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(schedule);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ScheduleExists(schedule.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["branch_id"] = new SelectList(_context.branches, "id", "id", schedule.branch_id);
+            return View(schedule);
+        }
+
+        // GET: Admin/Schedules/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.schedules
+                .Include(s => s.branch)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return View(schedule);
+        }
+
+        // POST: Admin/Schedules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var schedule = await _context.schedules.FindAsync(id);
+            _context.schedules.Remove(schedule);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ScheduleExists(int id)
+        {
+            return _context.schedules.Any(e => e.id == id);
+        }
+    }
+}
