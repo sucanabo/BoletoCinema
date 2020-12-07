5322084df3728f98c50b0bbe4ec6e1d93ba0f578
xuanloc <xuanloc.hcm2018@gmail.com>
2020-12-07 18:48:11 +0700

    addController+addMigration by xuanloc

---
+++ BoletoCinema/BoletoCinema/Areas/Admin/Controllers/RoomsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BoletoCinema.Areas.Admin.Data;
+using BoletoCinema.Areas.Admin.Models;
+
+namespace BoletoCinema.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoomsController : Controller
+    {
+        private readonly BoletoContext _context;
+
+        public RoomsController(BoletoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Rooms
+        public async Task<IActionResult> Index()
+        {
+            var boletoContext = _context.rooms.Include(r => r.branch);
+            return View(await boletoContext.ToListAsync());
+        }
+
+        // GET: Admin/Rooms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.rooms
+                .Include(r => r.branch)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // GET: Admin/Rooms/Create
+        public IActionResult Create()
+        {
+            ViewData["branch_id"] = new SelectList(_context.branches, "id", "id");
+            return View();
+        }
+
+        // POST: Admin/Rooms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,branch_id,name,status")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(room);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["branch_id"] = new SelectList(_context.branches, "id", "id", room.branch_id);
+            return View(room);
+        }
+
+        // GET: Admin/Rooms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.rooms.FindAsync(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            ViewData["branch_id"] = new SelectList(_context.branches, "id", "id", room.branch_id);
+            return View(room);
+        }
+
+        // POST: Admin/Rooms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,branch_id,name,status")] Room room)
+        {
+            if (id != room.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(room);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoomExists(room.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["branch_id"] = new SelectList(_context.branches, "id", "id", room.branch_id);
+            return View(room);
+        }
+
+        // GET: Admin/Rooms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.rooms
+                .Include(r => r.branch)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // POST: Admin/Rooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var room = await _context.rooms.FindAsync(id);
+            _context.rooms.Remove(room);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoomExists(int id)
+        {
+            return _context.rooms.Any(e => e.id == id);
+        }
+    }
+}
