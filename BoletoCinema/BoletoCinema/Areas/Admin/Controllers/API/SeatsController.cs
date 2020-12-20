3dec4a9ad001e99aaf0d25f0dd26a1cde77d7a71
Phuc <sucanabo@gmail.com>
2020-12-20 18:51:32 +0700

    done/chooseSeat

---
+++ BoletoCinema/BoletoCinema/Areas/Admin/Controllers/API/SeatsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BoletoCinema.Areas.Admin.Data;
+using BoletoCinema.Areas.Admin.Models;
+
+namespace BoletoCinema.Areas.Admin.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeatsController : ControllerBase
+    {
+        private readonly BoletoContext _context;
+
+        public SeatsController(BoletoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Seats
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Seat>>> Getseats()
+        {
+            return await _context.seats.ToListAsync();
+        }
+
+        // GET: api/Seats/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Seat>> GetSeat(int id)
+        {
+            var seat = await _context.seats.FindAsync(id);
+
+            if (seat == null)
+            {
+                return NotFound();
+            }
+
+            return seat;
+        }
+
+        // PUT: api/Seats/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSeat(int id, Seat seat)
+        {
+            if (id != seat.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(seat).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SeatExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Seats
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Seat>> PostSeat(Seat seat)
+        {
+            _context.seats.Add(seat);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSeat", new { id = seat.id }, seat);
+        }
+
+        // DELETE: api/Seats/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSeat(int id)
+        {
+            var seat = await _context.seats.FindAsync(id);
+            if (seat == null)
+            {
+                return NotFound();
+            }
+
+            _context.seats.Remove(seat);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SeatExists(int id)
+        {
+            return _context.seats.Any(e => e.id == id);
+        }
+    }
+}
